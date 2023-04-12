bd31120249a73f7aede8d5a3bf2331943ea817f2
Vitor Rocha <vitorlopeslrocha@gmail.com>
2023-04-12 13:55:55 -0300

    User index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Pack.create(name:"Kona", description:"Best Outdoor Pack", price:"100prata", photos:"sem foto")
+Pack.create(name:"Kona", description:"Best Outdoor Pack", price:"100prata", photos:"sem foto")
+
+Pack.create(name:"Kona", description:"Best Outdoor Pack", price:"100prata", photos:"sem foto")
+
+Pack.create(name:"Kona", description:"Best Outdoor Pack", price:"100prata", photos:"sem foto")
+
+Pack.create(name:"Kona", description:"Best Outdoor Pack", price:"100prata", photos:"sem foto")
+
+Pack.create(name:"Kona", description:"Best Outdoor Pack", price:"100prata", photos:"sem foto")
+
+Pack.create(name:"Kona", description:"Best Outdoor Pack", price:"100prata", photos:"sem foto")
