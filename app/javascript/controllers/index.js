92f4f984d0d611c9a36ed722ba332fd21b9bcfd4
Vitor Rocha <vitorlopeslrocha@gmail.com>
2023-05-13 11:46:25 -0300

    problemas com webpack resolvidos

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,15 +2,6 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from "./application"
-
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
-
-
-
-// app/javascript/controllers/index.js
-
 import { Application } from "stimulus"
 import Carousel from "stimulus-carousel"
 
